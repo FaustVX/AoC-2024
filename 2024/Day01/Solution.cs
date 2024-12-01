713ff97caaddf702fef0a2625c67a38e40ed9a6f
FaustVX <jodu035@gmail.com>
2024-12-01 11:17:34 +0100

    Initial commit for Y2024D1

---
+++ 2024/Day01/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2024.Day01;
+
+[ProblemInfo("Historian Hysteria")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+}
