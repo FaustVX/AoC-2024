4fe4ff1133a6af9b0af75c069b6ffa4f76384849
FaustVX <jodu035@gmail.com>
2024-12-02 20:24:56 +0100

    Solved P2 in 0:33:45

--- 2024/Day02/Solution.cs
+++ 2024/Day02/Solution.cs
@@ -9,64 +9,79 @@ using static System.MemoryExtensions;
 public class Solution : ISolver //, IDisplay
 {
     public object PartOne(ReadOnlyMemory<char> input)
-    {
-        var reports = Extensions.ParseToArray(input.Span, stackalloc Buffer[input.Span.Count('\n') + 1]);
-        var count = 0;
-        foreach (var item in reports)
-            if (item.IsValid())
-                count++;
-        return count;
-    }
+    => Execute(input.Span, b => b.IsValidP1());
 
     public object PartTwo(ReadOnlyMemory<char> input)
+    => Execute(input.Span, b => b.IsValidP2());
+
+    static int Execute(ReadOnlySpan<char> input, Func<Buffer, bool> isValid)
     {
-        return 0;
+        var reports = Extensions.ParseToArray(input, stackalloc Buffer[input.Count('\n') + 1]);
+        var count = 0;
+        foreach (var item in reports)
+            if (isValid(item))
+                count++;
+        return count;
     }
 }
 
 [System.Runtime.CompilerServices.InlineArray(9)]
-file struct Buffer : ISpanParsable<Buffer>
+struct Buffer : ISpanParsable<Buffer>
 {
     private int _element0;
 
     private static ReadOnlySpan<int> GetSpan(ref readonly Buffer buffer)
     => buffer[0..buffer[^1]];
 
-    public bool IsAscending()
+    public readonly bool IsAscending(int badLevel)
     {
-        var prev = this[0];
-        foreach (var item in GetSpan(ref this)[1..])
-            if (item <= prev)
+        var prev = int.MinValue;
+        for (int i = 0; i < this[^1]; i++)
+            if (i == badLevel)
+                continue;
+            else if (this[i] <= prev)
                 return false;
             else
-                prev = item;
+                prev = this[i];
         return true;
     }
 
-    public bool IsDescending()
+    public readonly bool IsDescending(int badLevel)
     {
-        var prev = this[0];
-        foreach (var item in GetSpan(ref this)[1..])
-            if (item >= prev)
+        var prev = int.MaxValue;
+        for (int i = 0; i < this[^1]; i++)
+            if (i == badLevel)
+                continue;
+            else if (this[i] >= prev)
                 return false;
             else
-                prev = item;
+                prev = this[i];
         return true;
     }
 
-    public bool HasAcceptableDistance()
+    public readonly bool HasAcceptableDistance(int badLevel)
     {
-        var prev = this[0];
-        foreach (var item in GetSpan(ref this)[1..])
-            if (Math.Abs(item - prev) is not (>= 1 and <= 3))
+        var prev = this[badLevel is 0 ? 1 : 0] - 1;
+        for (int i = 0; i < this[^1]; i++)
+            if (i == badLevel)
+                continue;
+            else if (Math.Abs(this[i] - prev) is not (>= 1 and <= 3))
                 return false;
             else
-                prev = item;
+                prev = this[i];
         return true;
     }
 
-    public bool IsValid()
-    => (IsAscending() || IsDescending()) && HasAcceptableDistance();
+    public readonly bool IsValidP1()
+    => (IsAscending(-1) || IsDescending(-1)) && HasAcceptableDistance(-1);
+
+    public readonly bool IsValidP2()
+    {
+        for (int i = 0; i < this[^1]; i++)
+            if ((IsAscending(i) || IsDescending(i)) && HasAcceptableDistance(i))
+                return true;
+        return false;
+    }
 
     public static Buffer Parse(ReadOnlySpan<char> s, IFormatProvider? provider)
     {
