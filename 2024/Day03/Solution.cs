f7e8c1137bc4fcd62df84dadb3d18eb9a3a7a7c4
FaustVX <jodu035@gmail.com>
2024-12-03 18:48:31 +0100

    Initial commit for Y2024D3

---
+++ 2024/Day03/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2024.Day03;
+
+[ProblemInfo("Mull It Over")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+}
