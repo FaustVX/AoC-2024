eaad38da0ddf26460ac5f5b6c35d7a1df0fb6d37
FaustVX <jodu035@gmail.com>
2024-12-04 21:25:00 +0100

    Initial commit for Y2024D4

---
+++ 2024/Day04/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2024.Day04;
+
+[ProblemInfo("Ceres Search")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+}
