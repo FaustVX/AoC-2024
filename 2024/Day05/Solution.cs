5b4a51b541815c8b40cf14088ccfbffa620ca9c1
FaustVX <jodu035@gmail.com>
2024-12-07 15:40:24 +0100

    Initial commit for Y2024D5

---
+++ 2024/Day05/Solution.cs
@@ -0,0 +1,16 @@
+#nullable enable
+namespace AdventOfCode.Y2024.Day05;
+
+[ProblemInfo("Print Queue")]
+public class Solution : ISolver //, IDisplay
+{
+    public object PartOne(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(ReadOnlyMemory<char> input)
+    {
+        return 0;
+    }
+}
